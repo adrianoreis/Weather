54373565c70ceabeb40a7316f2326678de35b7cc
Adriano Reis <reis.a@outlook.com>
2017-07-13 10:39:10 +0100

    Cache expires after one hour

--- src/main/java/com/develogical/CachedWeatherForecaster.java
+++ src/main/java/com/develogical/CachedWeatherForecaster.java
@@ -4,6 +4,8 @@ import com.weather.Day;
 import com.weather.Forecast;
 import com.weather.Region;
 
+import java.time.Clock;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,22 +16,40 @@ public class CachedWeatherForecaster implements WeatherForecaster {
 
     private static final int MAX_ENTRIES = 3;
     private final WeatherForecaster weatherForecaster;
-    private final Map<String, Forecast> cache = new HashMap<>();
+    private final Map<String, Map<Instant, Forecast>> cache = new HashMap<>();
+    private final Clock clock;
 
-    public CachedWeatherForecaster(WeatherForecaster weatherForecaster) {
+    public CachedWeatherForecaster(WeatherForecaster weatherForecaster, Clock clock) {
         this.weatherForecaster = weatherForecaster;
+        this.clock = clock;
     }
 
     @Override
     public Forecast weatherForecastFor(Region region, Day day) {
+        Instant now = clock.instant();
+
         if (cache.values().size() > MAX_ENTRIES){
             cache.clear();
         }
+
         if (cache.containsKey(region.name() + day.name())) {
-            return cache.get(region.name() + day.name());
+            Map<Instant, Forecast> val = cache.get(region.name() + day.name());
+            if (val.keySet().iterator().next().isAfter(now)) {
+                return val.values().iterator().next();
+            } else {
+                Map<Instant, Forecast> newForecast = new HashMap<>();
+                Forecast forecast = weatherForecaster.weatherForecastFor(region, day);
+                newForecast.put(now.plusSeconds(3600), forecast);
+                cache.replace(region.name()+day.name(), newForecast);
+                return forecast;
+            }
         }
+
         Forecast forecast = weatherForecaster.weatherForecastFor(region, day);
-        cache.put(region.name() + day.name(), forecast);
+
+        Map<Instant, Forecast> value = new HashMap<>();
+        value.put(now.plusSeconds(3600), forecast);
+        cache.put(region.name()+day.name(), value);
         return forecast;
     }
 }
--- src/test/java/com/develogical/CachedWeatherForecastTest.java
+++ src/test/java/com/develogical/CachedWeatherForecastTest.java
@@ -11,6 +11,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.temporal.TemporalUnit;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.times;
@@ -23,11 +27,13 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class CachedWeatherForecastTest {
     @InjectMocks
-    CachedWeatherForecaster cachedWeatherForecaster;
+    private CachedWeatherForecaster cachedWeatherForecaster;
     @Mock
-    WeatherForecaster weatherForecaster;
+    private WeatherForecaster weatherForecaster;
     @Mock
     private Forecast forecast;
+    @Mock
+    private Clock clock;
 
     @Before
     public void setUp() throws Exception {
@@ -36,12 +42,14 @@ public class CachedWeatherForecastTest {
 
     @Test
     public void testRetrieveForecast() throws Exception {
+        when(clock.instant()).thenReturn(Instant.now());
         Forecast forecastFor = cachedWeatherForecaster.weatherForecastFor(Region.LONDON, Day.FRIDAY);
         assertEquals(forecast, forecastFor);
     }
 
     @Test
     public void testRetrieveForecastFromCache() throws Exception {
+        when(clock.instant()).thenReturn(Instant.now(), Instant.now().plusSeconds(60));
         cachedWeatherForecaster.weatherForecastFor(Region.LONDON, Day.FRIDAY);
         cachedWeatherForecaster.weatherForecastFor(Region.LONDON, Day.FRIDAY);
         verify(weatherForecaster).weatherForecastFor(Region.LONDON, Day.FRIDAY);
@@ -49,10 +57,22 @@ public class CachedWeatherForecastTest {
 
     @Test
     public void testCacheIsResetAfterThreeEntries() throws Exception {
+        when(clock.instant()).thenReturn(Instant.now());
         cachedWeatherForecaster.weatherForecastFor(Region.LONDON, Day.FRIDAY);
         cachedWeatherForecaster.weatherForecastFor(Region.LONDON, Day.SATURDAY);
         cachedWeatherForecaster.weatherForecastFor(Region.LONDON, Day.SUNDAY);
         cachedWeatherForecaster.weatherForecastFor(Region.LONDON, Day.MONDAY);
+        cachedWeatherForecaster.weatherForecastFor(Region.LONDON, Day.FRIDAY);
+        verify(weatherForecaster, times(2)).weatherForecastFor(Region.LONDON, Day.FRIDAY);
+    }
+
+    @Test
+    public void testCachedValueExpiresAfterOneHour() throws Exception {
+
+        Instant now = Instant.now();
+        when(clock.instant()).thenReturn(now,now.plusSeconds(1800),now.plusSeconds(3601));
+        cachedWeatherForecaster.weatherForecastFor(Region.LONDON, Day.FRIDAY);
+        cachedWeatherForecaster.weatherForecastFor(Region.LONDON, Day.FRIDAY);
         cachedWeatherForecaster.weatherForecastFor(Region.LONDON, Day.FRIDAY);
         verify(weatherForecaster, times(2)).weatherForecastFor(Region.LONDON, Day.FRIDAY);
     }
